a9319a0a4d96ed4f86921c1bbf6764f15d334b2c
lxyu <lixinfish@gmail.com>
2012-08-29 19:13:05 +0800

    make it a pypi package.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='drawsomething',
+    version='0.1.0',
+    description='Drawsomething words guess helper.',
+    author='Lx Yu',
+    author_email='lixinfish@gmail.com',
+    packages=['drawsomething', ],
+    package_data={'': ['LICENSE'], },
+    scripts=['bin/drawsth'],
+    url='https://github.com/lxyu/drawsomething',
+    license=open('LICENSE').read(),
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "pyenchant",
+        "google_translate",
+    ],
+)
